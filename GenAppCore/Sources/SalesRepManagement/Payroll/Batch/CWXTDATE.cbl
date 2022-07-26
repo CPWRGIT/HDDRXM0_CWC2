e06ab0704ca83d995e42d7ef575d806b961addd4
stupot1966 <stuart_ashby@bmc.com>
2022-07-26 13:50:28 +0100

    FT1 shakedown 13:50 BST

--- GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTDATE.cbl
+++ GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTDATE.cbl
@@ -10,6 +10,7 @@
       * Test 2022/07/06                                                *
       * Test 2022/07/22                                                *
       * Test 2022/07/25 - CLI 20.10.01                                 *
+      * Test 2022/07/26 - FT1 shakedown                                *
       ******************************************************************
       * Jenkins version testing 
        ENVIRONMENT DIVISION.
