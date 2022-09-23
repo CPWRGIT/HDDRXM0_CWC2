5c42bbcaf740ddaff7b190b3e42c3c6b8ac59d09
manosing <manosing@AM-manosing-W3.adprod.bmc.com>
2022-09-23 12:49:16 -0500

    updated-MKS

--- GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTDATE.cbl
+++ GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTDATE.cbl
@@ -7,7 +7,7 @@
       *  THIS CALLED PROGRAM IS PART OF THE COBOL DEMO PROGRAM USED    *
       *  FOR XPEDITER/TSO TRAINING SESSIONS                            *
       *                                                                *
-      * Test 2022/07/06                                                *
+      * Test 2022/07/05                                                *
       * Test 2022/07/22                                                *
       * Test 2022/07/25 - CLI 20.10.01                                 *
       * Test 2022/07/26 - FT1 shakedown                                *
